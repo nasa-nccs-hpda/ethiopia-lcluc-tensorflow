31aae3790d5ee5be31f80292f7aca47e5af54bad
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2021-11-16 14:55:23 -0500

    Adding gpu pipeline:

--- projects/land_cover/rf_pipeline_gpu.py
+++ projects/land_cover/rf_pipeline_gpu.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-CPU and GPU Random Forest Pipeline - preprocess, train, predict.
+GPU Random Forest Pipeline - preprocess, train, predict.
 Author: Jordan Alexis Caraballo-Vega, jordan.a.caraballo-vega@nasa.gov
 """
-
 import os
 import gc
 import sys
@@ -17,26 +16,20 @@ import joblib
 import numpy as np
 import xarray as xr
 import pandas as pd
+from osgeo import gdal, osr
 import rasterio as rio
 import rasterio.features as riofeat
-from osgeo import gdal, osr
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier as sklRFC
+import cupy
+import cudf
+import cuml
 
-from sklearn.metrics import accuracy_score, \
-    precision_score, recall_score, f1_score
+from cuml.ensemble import RandomForestClassifier as cumlRFC
+from cuml.model_selection import train_test_split
+from cuml.metrics import accuracy_score
+from cupyx.scipy.ndimage import median_filter
 
-try:
-    import cupy as cp
-    import cudf as cf
-    from cuml.ensemble import RandomForestClassifier as cumlRFC
-    # from cuml.dask.ensemble import RandomForestClassifier as cumlRFC_mg
-    from cupyx.scipy.ndimage import median_filter
-    cp.random.seed(seed=None)
-    HAS_GPU = True
-except ImportError:
-    HAS_GPU = False
+cupy.random.seed(seed=24)
 
 __author__ = "Jordan A Caraballo-Vega, Science Data Processing Branch"
 __email__ = "jordan.a.caraballo-vega@nasa.gov"
@@ -135,39 +128,26 @@ def main():
     parser = argparse.ArgumentParser(description=desc)
 
     parser.add_argument(
-        '--gpu', dest='has_gpu', action='store_true', default=False)
-
-    parser.add_argument(
         '--output-model', type=str, required=False,
         dest='output_pkl', help='Path to the output PKL file (.pkl)')
-
-    parser.add_argument(
-        '--data-csv', type=str, required=False,
-        dest='data_csv', help='Path to the data CSV configuration file')
 
     parser.add_argument(
         '--train-csv', type=str, required=False,
         dest='train_csv', help='Path to the output CSV file')
 
     parser.add_argument(
-        '--bands', type=str, nargs='*', required=False,
-        dest='bands', help='Bands to store in CSV file',
-        default=['CoastalBlue', 'Blue', 'Green', 'Yellow',
-                 'Red', 'RedEdge', 'NIR1', 'NIR2'])
-
-    parser.add_argument(
         '--step', type=str, nargs='*', required=True,
         dest='pipeline_step', help='Pipeline step to perform',
-        default=['preprocess', 'train', 'predict', 'vis'],
-        choices=['preprocess', 'train', 'predict', 'vis'])
+        default=['train', 'predict', 'vis'],
+        choices=['train', 'predict', 'vis'])
 
     parser.add_argument(
         '--seed', type=int, required=False, dest='seed',
         default=42, help='Random SEED value')
 
     parser.add_argument(
-        '--test-size', type=float, required=False,
-        dest='test_size', default=0.20, help='Test size rate (e.g .30)')
+        '--train-size', type=float, required=False,
+        dest='train_size', default=0.80, help='Train size rate (e.g .30)')
 
     parser.add_argument(
         '--n-trees', type=int, required=False,
@@ -210,91 +190,6 @@ def main():
     logger.addHandler(ch)
 
     # --------------------------------------------------------------------------------
-    # preprocess step
-    # --------------------------------------------------------------------------------
-    if "preprocess" in args.pipeline_step:
-
-        # ----------------------------------------------------------------------------
-        # 1. Read data csv file
-        # ----------------------------------------------------------------------------
-        assert os.path.exists(args.data_csv), f'{args.data_csv} not found.'
-        data_df = pd.read_csv(args.data_csv)  # initialize data dataframe
-        assert not data_df.isnull().values.any(), f'NaN found: {args.data_csv}'
-        logging.info(f'Open {args.data_csv} for preprocessing.')
-
-        # ----------------------------------------------------------------------------
-        # 2. Extract points out of spatial imagery - rasters
-        # ----------------------------------------------------------------------------
-        # set empty dataframe to store point values
-        list_points = []
-        df_points = pd.DataFrame(columns=args.bands + ['CLASS'])
-        logging.info(f"Generating {data_df['ntiles'].sum()} points dataset.")
-
-        # start iterating over each file
-        for di in data_df.index:
-
-            # get filename for output purposes, in the future, add to column
-            filename = data_df['data'][di].split('/')[-1]
-            logging.info(f'Processing {filename}')
-
-            # read imagery from disk and process both image and mask
-            img = xr.open_rasterio(data_df['data'][di]).values
-            mask = xr.open_rasterio(data_df['label'][di]).values
-
-            # ------------------------------------------------------------------------
-            # Unique processing of this project - Start
-            # ------------------------------------------------------------------------
-            # squeeze mask if needed, start classes fro 0-n_classes
-            mask = np.squeeze(mask) if len(mask.shape) != 2 else mask
-            mask = mask - 1 if np.min(mask) == 1 else mask
-            # ------------------------------------------------------------------------
-            # Unique processing of this project - Done
-            # ------------------------------------------------------------------------
-
-            # crop ROI, from outside to inside based on pixel address
-            ymin, ymax = data_df['ymin'][di], data_df['ymax'][di]
-            xmin, xmax = data_df['xmin'][di], data_df['xmax'][di]
-            img, mask = \
-                img[:, ymin:ymax, xmin:xmax], mask[ymin:ymax, xmin:xmax]
-
-            # crop ROI, from outside to inside based on pixel value
-            # img = np.clip(img, 0, 10000)
-
-            # get N points from imagery
-            points_per_class = data_df['ntiles'][di] // args.n_classes
-            logging.info(f'Generating {points_per_class} points per class.')
-
-            # extract values from imagery, two classes
-            for cv in range(args.n_classes):
-
-                logging.info(f'Starting with class: {cv}')
-                bbox = img.shape  # size of the image
-                counter = 0  # counter for class balancing
-
-                while counter < points_per_class:
-
-                    # get indices and extract spectral and class value
-                    y, x = random.randrange(bbox[1]), random.randrange(bbox[2])
-                    sv, lv = img[:, y, x], int(mask[y, x])
-
-                    if lv == cv:
-                        # trying speed up here - looks like from list is faster
-                        list_points.append(
-                            pd.DataFrame(
-                                [np.append(sv, [lv])],
-                                columns=list(df_points.columns))
-                        )
-                        counter += 1
-
-        df_points = pd.concat(list_points)
-
-        # ----------------------------------------------------------------------------
-        # 3. Save file to disk
-        # ----------------------------------------------------------------------------
-        df_points.to_csv(args.train_csv, index=False)
-        logging.info(f'Saved dataset file {args.train_csv}')
-
-    # --------------------------------------------------------------------------------
     # train step
     # --------------------------------------------------------------------------------
     if "train" in args.pipeline_step:
@@ -303,7 +198,7 @@ def main():
         # 1. Read data csv file
         # ----------------------------------------------------------------------------
         assert os.path.exists(args.train_csv), f'{args.train_csv} not found.'
-        data_df = pd.read_csv(args.train_csv, sep=',')
+        data_df = cudf.read_csv(args.train_csv, sep=',')
         assert not data_df.isnull().values.any(), f'Na found: {args.train_csv}'
         logging.info(f'Open {args.train_csv} dataset for training.')
 
@@ -318,7 +213,7 @@ def main():
 
         # split data into training and test
         x_train, x_test, y_train, y_test = train_test_split(
-            x, y, test_size=args.test_size, random_state=args.seed)
+            x, y, train_size=args.train_size)
         del data_df, x, y
 
         # logging some of the model information
@@ -326,75 +221,19 @@ def main():
         logging.info(f'ntrees={str(args.n_trees)}, maxfeat={args.max_feat}')
 
         # ------------------------------------------------------------------
-        # 3. Instantiate RandomForest object - FIX this area
+        # 3. Instantiate RandomForest object
         # ------------------------------------------------------------------
-        if args.has_gpu:  # run using RAPIDS library
-
-            # initialize cudf data and log into GPU memory
-            logging.info('Training model via RAPIDS.')
-
-            # single gpu setup
-            x_train = cf.DataFrame.from_pandas(x_train)
-            x_test = cf.DataFrame.from_pandas(x_test)
-            y_train = cf.Series(y_train.values)
-            rf_funct = cumlRFC  # RF Classifier
-
-            # TODO: multi gpu setup
-            # https://github.com/rapidsai/cuml/blob/branch-21.12/notebooks/random_forest_mnmg_demo.ipynb
-            # cluster = LocalCUDACluster(
-            # threads_per_worker=1, n_workers=n_workers)
-            # c = Client(cluster)
-            # workers = c.has_what().keys()
-            # rf_funct = cumlRFC_mg
-
-            # Shard the data across all workers
-            # X_train_df, y_train_df = dask_utils.persist_across_workers(
-            # c,[X_train_df,y_train_df],workers=workers)
-
-            # Build and train the model
-            # cu_rf_mg = cuRFC_mg(**cu_rf_params)
-            # cu_rf_mg.fit(X_train_df, y_train_df)
-
-            # Check the accuracy on a test set
-            # cu_rf_mg_predict = cu_rf_mg.predict(X_test)
-            # acc_score = accuracy_score(
-            # cu_rf_mg_predict, y_test, normalize=True)
-            # c.close()
-            # cluster.close()
-
-        else:
-            logging.info('Training model via SKLearn.')
-            rf_funct = sklRFC
-
-        # Initialize model
-        rf_model = rf_funct(
+        rf_model = cumlRFC(
             n_estimators=args.n_trees, max_features=args.max_feat)
 
-        # ------------------------------------------------------------------
-        # 4. Fit Model
-        # ------------------------------------------------------------------
         # fit model to training data and predict for accuracy score
         rf_model.fit(x_train, y_train)
 
-        if args.has_gpu:
-            acc_score = accuracy_score(
-                y_test, rf_model.predict(x_test).to_array())
-            p_score = precision_score(
-                y_test, rf_model.predict(x_test).to_array(), average='macro')
-            r_score = recall_score(
-                y_test, rf_model.predict(x_test).to_array(), average='macro')
-            f_score = f1_score(
-                y_test, rf_model.predict(x_test).to_array(), average='macro')
-        else:
-            acc_score = accuracy_score(y_test, rf_model.predict(x_test))
-            p_score = precision_score(y_test, rf_model.predict(x_test), average='macro')
-            r_score = recall_score(y_test, rf_model.predict(x_test), average='macro')
-            f_score = f1_score(y_test, rf_model.predict(x_test), average='macro')
-
+        # ------------------------------------------------------------------
+        # 4. Predict test set for accuracy metrics
+        # ------------------------------------------------------------------
+        acc_score = accuracy_score(y_test, rf_model.predict(x_test).to_array())
         logging.info(f'Test Accuracy:  {acc_score}')
-        logging.info(f'Test Precision: {p_score}')
-        logging.info(f'Test Recall:    {r_score}')
-        logging.info(f'Test F-Score:   {f_score}')
 
         # make output directory
         os.makedirs(
