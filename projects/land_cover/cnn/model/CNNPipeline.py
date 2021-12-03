efc08fb2c2cd40595f0e44026174adb65ff0e528
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2021-12-02 20:56:17 -0500

    testing

--- projects/land_cover/cnn/model/CNNPipeline.py
+++ projects/land_cover/cnn/model/CNNPipeline.py
@@ -253,8 +253,9 @@ class Train(Config):
 
         # Loss and Optimizer
         # self.criterion = nn.CrossEntropyLoss().to(self.device)
-        self.criterion = mIoULoss(n_classes=self.n_classes).to(self.device)
+        # self.criterion = mIoULoss(n_classes=self.n_classes).to(self.device)
         # self.criterion = FocalLoss().to(self.device)
+        self.criterion = nn.CrossEntropyLoss(ignore_index=0).to(self.device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.0001)
         self.scheduler = torch.optim.lr_scheduler.StepLR(
             self.optimizer, step_size=1, gamma=0.5)
