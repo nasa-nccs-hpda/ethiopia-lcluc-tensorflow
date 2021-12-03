c65884ec102b0c7dd72c76b1faab3898b711a8ab
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2021-12-03 08:00:50 -0500

    Debugging

--- projects/land_cover/cnn/model/CNNPipeline.py
+++ projects/land_cover/cnn/model/CNNPipeline.py
@@ -470,7 +470,7 @@ class Predict(Preprocess):
             # Normalize accumulated mask and convert back to numpy
             merged_mask = np.moveaxis(
                 to_numpy(merger.merge()), 0, -1).astype(np.uint8)
-            print("UNIQUE IN MASK: ", np.unique(merged_mask))
+            print("UNIQUE IN MASK: ", np.unique(merged_mask), merged_mask.shape)
             merged_mask = tiler.crop_to_orignal_size(merged_mask)
             merged_mask = np.squeeze(merged_mask, axis=-1)
             print("UNIQUE IN MASK: ", np.unique(merged_mask))
